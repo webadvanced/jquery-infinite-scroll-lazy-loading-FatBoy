66e33cd8613a78429d5e0783b84b45540d201d5d
Web Advanced <paul.barton@webadvanced.com>
2012-09-18 12:04:45 -0700

    new build

--- src/fatBoy.js
+++ src/fatBoy.js
@@ -2,12 +2,13 @@
 	if( $ === _undefined ) throw 'jQuery is required. Please make sure you include a reference to jQuery and that this script is included below it.';
 
 	var defaults = {
-		limit: 3,                   // Number of times to fire event
-		threshold: 25,              // Number of px up from the bottom of the page
-		triggerEvent: 'nextPage',   // Event that jQuery will trigger when user reaches the bottom of the page
-		uiEvent: 'scroll',          // User event to trigger plugin
-		callback: _undefined,       // Callback function to be executed when user reaches the bottom of the page
-		limitReached: _undefined    // Called when limit is reached
+		limit: 3,                             // Number of times to fire event
+		threshold: 25,                        // Number of px up from the bottom of the page
+		triggerEvent: 'nextPage',             // Event that jQuery will trigger when user reaches the bottom of the page
+		uiEvent: 'scroll',                    // User event to trigger plugin
+		callback: _undefined,                 // Callback function to be executed when user reaches the bottom of the page
+		limitReached: _undefined,             // Called when limit is reached
+		limitReachedEvent: 'limitReached',    // Called when limit is reached
 	}, 
 		atBottom,
 		$window = $( w ),
@@ -35,13 +36,19 @@
 				//If the limit has been reach, unbind the scrool event from the container
 				if( options.limit !== 0 && count >= options.limit ) {	
 					$container.unbind( options.uiEvent );
-					if( options.limitReached ) options.limitReached();
+					if( !options.limitReached ) return;
+					$container.trigger( options.limitReachedEvent );
 				}
 			}
 		});
 		//If a callback is provided, bind it to the triggerEvent
 		if( options.callback ) {
 			$container.bind( options.triggerEvent, options.callback );
+		}
+
+		//If limitReached callback is provided
+		if( options.limitReached ) {
+			$container.bind( options.limitReachedEvent, options.limitReached );
 		}
 	};
 
